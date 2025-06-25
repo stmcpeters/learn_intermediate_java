4dcab474b43d091b33ae374dcd6d183816ec6716
stmcpeters <stmcpeters@gmail.com>
2025-06-24 19:17:06 -0700

    Implement Food class and PricedItem interface with price management

--- takeout_project/Food.java
+++ takeout_project/Food.java
@@ -1,1 +1,28 @@
-// Create Food class here
+public class Food implements PricedItem<Integer> {
+//    instance fields
+    private final String name;
+    private final String description;
+    private int price;
+
+//    constructor
+    public Food(String name, String description, int price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public Integer getPrice(){
+        return this.price;
+    }
+
+    @Override
+    public void setPrice(Integer price){
+        this.price = price;
+    }
+
+    @Override
+    public String toString(){
+        return "Enjoy " + this.name + ": " + this.description + "     Cost: $" + this.price;
+    }
+}
--- takeout_project/PricedItem.java
+++ takeout_project/PricedItem.java
@@ -1,1 +1,4 @@
-// Create PricedItem interface here
+public interface PricedItem <T extends Number> {
+    T getPrice();
+    void setPrice(T price);
+}
