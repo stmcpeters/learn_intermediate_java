dd3a1f7f0ee88653a112709f9e39050360cb3e96
stmcpeters <stmcpeters@gmail.com>
2025-06-24 17:15:22 -0700

    Add Customer class with fields, constructor, and getter/setter methods

--- takeout_project/Customer.java
+++ takeout_project/Customer.java
@@ -1,1 +1,33 @@
-// Create Customer class here
+class Customer {
+//    instance fields
+    private String name;
+    private int money;
+
+//    constructor
+    public Customer(String name, int money){
+        this.name = name;
+        this.money = money;
+    }
+
+//    toString()
+    @Override
+    public String toString(){
+        return "Customer named " + this.name + " has $" + this.money;
+    }
+
+//    getters
+    public String getCustomerName() {
+        return this.name;
+    }
+    public int getCustomerMoney(){
+        return this.money;
+    }
+
+//    setters
+    public void setCustomerName(String name) {
+        this.name = name;
+    }
+    public void setCustomerMoney(int money) {
+        this.money = money;
+    }
+}
