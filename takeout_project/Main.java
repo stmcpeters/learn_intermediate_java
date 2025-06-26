37ae46affe54ffcd9516465e24aa9b3fbb052443
stmcpeters <stmcpeters@gmail.com>
2025-06-25 17:45:21 -0700

    Implement TakeOutSimulator class for managing customer interactions and food ordering process

--- takeout_project/Main.java
+++ takeout_project/Main.java
@@ -2,9 +2,26 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    FoodMenu menu = new FoodMenu();
-    System.out.println(menu);
-    System.out.println(menu.getLowestCostFood());
-  }    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String customerName = input.nextLine();
+
+        int money = 0;
+        while (true) {
+            System.out.print("Enter your starting money: ");
+            try {
+                money = input.nextInt();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please input a valid int input");
+                input.next(); // clear invalid input
+            }
+        }
+
+        Customer customer = new Customer(customerName, money);
+        TakeOutSimulator takeOutSimulator = new TakeOutSimulator(customer, input);
+        takeOutSimulator.startTakeOutSimulator();
+    }
 }
