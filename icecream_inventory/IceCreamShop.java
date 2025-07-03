c0c87ef955fdcef480740bfca3d31d5c8683da89
stmcpeters <stmcpeters@gmail.com>
2025-07-03 16:12:27 -0700

    Refactor order processing in IceCreamShop to utilize increaseCount methods for item management

--- icecream_inventory/IceCreamShop.java
+++ icecream_inventory/IceCreamShop.java
@@ -55,25 +55,36 @@ public class IceCreamShop {
     System.out.println("Hello! Welcome to the ice cream shop. We are ready to take your order. For ice-cream type 1, for shakes type 2, for smoothies type 3, and for slushies type 4. If you are done ordering type 0.");
     orderType = input.nextInt();
 
+    ShopItems items = new ShopItems();
+    ShopItems.NonVegan nonVegan = items.new NonVegan();
+    ShopItems.Vegan vegan = items.new Vegan();
+
     while(orderType != 0) {
       System.out.println("How many items of this item would you like to order?");
       itemCount = input.nextInt();
       if(orderType == 1){
         itemName = "ice cream";
+        nonVegan.increaseCount(1, itemCount);
       } else if(orderType == 2) {
         itemName = "shake";
+        nonVegan.increaseCount(2, itemCount);
       } else if(orderType == 3) {
         itemName = "smoothie";
+        vegan.increaseCount(3, itemCount);
       } else if(orderType == 4) {
         itemName = "slushie";
+        vegan.increaseCount(4, itemCount);
       } else {
         break;
       }
 
       System.out.println("Adding " + itemCount + " of the item "+ itemName + " to your order!");
 
-      System.out.println("Type a number for the next item in your order:\n1: Ice cream\n2: Shake\n3: Smoothie\n4: Slushie\n");
+      System.out.println("Type a number for the next item in your order:\n1: Ice cream\n2: Shake\n3: Smoothie\n4: Slushie\nEnter 0 to quit\n");
       orderType = input.nextInt();
     }
+      System.out.println("Thank you for your order! Today we have sold " + items.totalCount + " orders of sweetness!");
+      System.out.println(nonVegan.totalCount + " items have been ice cream and shakes.");
+      System.out.println(vegan.totalCount + " items have been smoothies and slushies.");
   }
 }
