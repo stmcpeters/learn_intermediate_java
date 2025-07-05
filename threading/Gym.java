b72d2bc250f8de63e1e1ac0d2a9e3e23fe1d569a
stmcpeters <stmcpeters@gmail.com>
2025-07-05 14:40:37 -0700

    Implement Gym class with member count and available machines

--- threading/Gym.java
+++ threading/Gym.java
@@ -1,4 +1,11 @@
+import java.util.Map;
 
 public class Gym {
+  private final int totalGymMembers;
+  private Map<MachineType, Integer> availableMachines;
 
+  public Gym(int totalGymMembers, Map<MachineType, Integer> availableMachines){
+    this.totalGymMembers = totalGymMembers;
+    this.availableMachines = availableMachines;
+  }
 }
