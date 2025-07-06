a66a5f95688735d45e58cd060d858ea24cb97853
stmcpeters <stmcpeters@gmail.com>
2025-07-05 17:52:20 -0700

    add method to open gym for the day and manage member routines

--- threading/Gym.java
+++ threading/Gym.java
@@ -1,4 +1,7 @@
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Gym {
   private final int totalGymMembers;
@@ -7,5 +10,22 @@ public class Gym {
   public Gym(int totalGymMembers, Map<MachineType, Integer> availableMachines){
     this.totalGymMembers = totalGymMembers;
     this.availableMachines = availableMachines;
+  }
+
+  public void openForTheDay(){
+    List<Thread> gymMembersRoutines;
+    gymMembersRoutines = IntStream.rangeClosed(1, this.totalGymMembers).mapToObj(
+      (id) -> {
+        Member member = new Member(id);
+        return new Thread(() -> {
+          try {
+            member.performRoutine();
+          } catch (Exception e) {
+            System.out.println(e);
+          }
+        });
+    }).collect(Collectors.toList());
+
+    gymMembersRoutines.forEach(Thread::start);
   }
 }
